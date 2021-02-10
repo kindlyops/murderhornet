f786a733ef6afd1e986eec05c2362c005433679b
Christopher Mundus <chris@kindlyops.com>
2021-02-10 12:29:18 -0500

    Adds react to the project
    Now using react for the project.
    
    Signed-off-by: Christopher Mundus <chris@kindlyops.com>

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import './index.css';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { LoginWithSSOButton } from './components/sso';
+
+
+
+
+ReactDOM.render(<LoginWithSSOButton />, document.getElementById('login'));
