0dbe379645ba0dedfb7f1d09452556888c6bbd93
Elliot Murphy <statik@users.noreply.github.com>
2021-02-18 16:53:47 -0500

    fluent-ffmpeg webpack hax
    https://github.com/fluent-ffmpeg/node-fluent-ffmpeg/issues/573
    
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,7 +1,11 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = [
   new ForkTsCheckerWebpackPlugin({
     async: false
-  })
+  }),
+  new webpack.DefinePlugin({
+        'process.env.FLUENTFFMPEG_COV': false
+    })
 ];
